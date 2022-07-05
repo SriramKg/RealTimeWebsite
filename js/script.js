167bc6eb1202c96cf1f92eddb4ec87c71730c6fd
Sriram <kgsriram99@gmail.com>
2022-07-05 21:25:38 +0530

    using ajax

--- js/script.js
+++ js/script.js
@@ -9,3 +9,24 @@ $(function () { // similar to DOMContentOverloaded in JS
         
     });
 });
+
+(function (global) {
+    var dc = {};
+    var homeHTML = "snippets/home-snippet.html";
+    var insertHTML = function (selector, html) {
+        var targetElement = document.querySelector(selector);
+        targetElement.innerHTML = html;
+    };
+    var showLoading = function (selector) {
+        var html = "<div class = 'text-center'>";
+        html += "<img src='ajax-loader.gif'></div>";
+        insertHTML(selector,html);
+    };
+    document.addEventListener("DOMContentLoaded", function (event) {
+        showLoading("#main-content");
+        $ajaxUtils.sendGetRequest(homeHTML, function (responseText) {
+            document.querySelector("#main-content").innerHTML = responseText;
+        }, false);
+    });
+    global.$dc = dc;
+})(window);
